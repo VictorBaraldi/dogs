c8d72f0f1e5e32f08cc5b80d22c4e91bfe23de13
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2023-03-07 21:10:38 -0300

    first commit

---
+++ src/api/Api.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserPosts from './endpoints/UserPosts';
+
+const Api = () => {
+  return (
+    <div>
+      <h2>USER POST</h2>
+      <UserPosts />
+    </div>
+  );
+};
+
+export default Api;
