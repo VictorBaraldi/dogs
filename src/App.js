0d6d69bec8358056510f11bc88ac97871d107226
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2023-03-09 15:25:52 -0300

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import Api from './api/Api';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
+import './styles/App.css';
 
 function App() {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
