579f28e8d9aa26b4af28241626036b1ba8e4ca5b
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2023-03-16 17:21:43 -0300

    Modal

--- src/Feed/FeedModal.js
+++ src/Feed/FeedModal.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { PHOTO_GET } from '../api';
+import Error from '../components/Helper/Error';
+import Loading from '../components/Helper/Loading';
+import PhotoContent from '../components/Photo/PhotoContent';
+import UseFetch from '../Hooks/UseFetch';
+import styles from '../styles/feed/Modal.module.css';
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = UseFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  function handleOutsideClick(e) {
+    if (e.target === e.currentTarget) setModalPhoto(null);
+  }
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error erro={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
