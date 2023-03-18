5186d79553615fb246d7753fdbbb493630f2a60e
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2023-03-18 19:35:23 -0300

    Skeleton

--- src/UserContext.js
+++ src/UserContext.js
@@ -12,39 +12,16 @@ export const UserStorage = ({ children }) => {
   const navigate = useNavigate();
 
   const userLogout = React.useCallback(
-    async function userLogout() {
+    async function () {
       setData(null);
       setError(null);
-      setLogin(false);
       setLoading(false);
+      setLogin(false);
       window.localStorage.removeItem('token');
       navigate('/login');
     },
     [navigate],
   );
-
-  React.useEffect(() => {
-    async function autoLogin() {
-      const token = window.localStorage.getItem('token');
-      if (token) {
-        try {
-          setError(null);
-          setLoading(true);
-          const { url, options } = TOKEN_VALIDATE_POST(token);
-          const response = await fetch(url, options);
-          if (!response.ok) throw new Error('Token inválido');
-          await getUser(token);
-        } catch (erro) {
-          userLogout();
-        } finally {
-          setLoading(false);
-        }
-      } else {
-        setLogin(false);
-      }
-    }
-    autoLogin();
-  }, [userLogout]);
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
@@ -59,19 +36,44 @@ export const UserStorage = ({ children }) => {
       setError(null);
       setLoading(true);
       const { url, options } = TOKEN_POST({ username, password });
-      const response = await fetch(url, options);
-      if (!response.ok) throw new Error(`Error: Usuário inválido`);
-      const json = await response.json();
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token);
+      const tokenRes = await fetch(url, options);
+      if (!tokenRes.ok) throw new Error(`Error: Usuário ou senha inválido.`);
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem('token', token);
+      await getUser(token);
       navigate('/conta');
-    } catch (erro) {
-      setError(erro.message);
+    } catch (err) {
+      setError(err.message);
       setLogin(false);
     } finally {
       setLoading(false);
     }
   }
+
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem('token');
+
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error('Token inválido.');
+          await getUser(token);
+        } catch (err) {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        setLogin(false);
+      }
+    }
+
+    autoLogin();
+  }, [userLogout]);
 
   return (
     <UserContext.Provider
